038eff71cefe68d8730a9330ebfc4d6a4fc340a0
EnjoyTheNoise <jankos.pdp@gmail.com>
2017-07-30 15:25:46 +0200

    code cleanup

--- RSSFeedReader/MainWindow.xaml.cs
+++ RSSFeedReader/MainWindow.xaml.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Navigation;
 using System.Xml;
-using System.ServiceModel.Syndication;
-using System.Windows.Media;
 using System.Xml.Linq;
 
 namespace RSSFeedReader
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string _url = "https://nyaa.si/?page=rss";
+        private string _url = "https://nyaa.si/?page=rss";      //default path
         private SyndicationFeed _feed;
 
         public MainWindow()
@@ -26,12 +25,7 @@ namespace RSSFeedReader
             CreateFeed();
         }
 
-        private void RefBtn_Click(object sender, RoutedEventArgs e)
-        {
-            CreateFeed();
-        }
-
-        private void CreateFeed()
+        private void CreateFeed()                               //create syndicationfeed from given url
         {
             try
             {
@@ -56,13 +50,18 @@ namespace RSSFeedReader
             }
         }
 
+        private void RefBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CreateFeed();
+        }
+
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
         }
 
-        private void Update(object sender, SelectionChangedEventArgs e)
+        private void Update(object sender, SelectionChangedEventArgs e)                 //listbox selection change event
         {
             if (e.AddedItems.Count == 0)
             {
@@ -75,13 +74,21 @@ namespace RSSFeedReader
             var item = e.AddedItems[0] as SyndicationItem;
             if (item == null) return;
 
-            var selectedData = item.ElementExtensions.Where(x => x.OuterName == "seeders").Select(y => y.GetObject<XElement>().Value).ToList();
+            var selectedData =
+                item.ElementExtensions.Where(x => x.OuterName == "seeders")
+                    .Select(y => y.GetObject<XElement>().Value)
+                    .ToList();
             SeedersTextBox.Text = selectedData[0];
-            selectedData = item.ElementExtensions.Where(x => x.OuterName == "leechers").Select(y => y.GetObject<XElement>().Value).ToList();
+            selectedData =
+                item.ElementExtensions.Where(x => x.OuterName == "leechers")
+                    .Select(y => y.GetObject<XElement>().Value)
+                    .ToList();
             LeechersTextBlock.Text = selectedData[0];
-            selectedData = item.ElementExtensions.Where(x => x.OuterName == "downloads").Select(y => y.GetObject<XElement>().Value).ToList();
+            selectedData =
+                item.ElementExtensions.Where(x => x.OuterName == "downloads")
+                    .Select(y => y.GetObject<XElement>().Value)
+                    .ToList();
             DownloadsTextBlock.Text = selectedData[0];
-            
         }
 
         private void DlBtn_OnClick(object sender, RoutedEventArgs e)
@@ -94,13 +101,11 @@ namespace RSSFeedReader
         private void SearchBtn_Click(object sender, RoutedEventArgs e)
         {
             if (QueryTextBox.Text != null)
-            {
                 _url = "https://nyaa.si/?page=rss&q=" + QueryTextBox.Text + "&c = 0_0 & f = 0";
-            }
             CreateFeed();
         }
 
-        private void OnLoad(object sender, RoutedEventArgs e)
+        private void OnLoad(object sender, RoutedEventArgs e)                               //search textbox default text
         {
             QueryTextBox.FontStyle = FontStyles.Italic;
             QueryTextBox.FontSize = 12;
